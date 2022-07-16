4fbb36ce6d30f01d555de097fd73887e14b95b73
Sem Rekkers <rekkers.sem@gmail.com>
2022-07-16 02:38:52 +0200

    Move misc to root of library

--- sqlite3_opt_misc.go
+++ sqlite3_opt_misc.go
@@ -3,10 +3,17 @@
 
 package sqlite3
 
-import (
-	"github.com/semrekkers/go-sqlite3/ext/misc"
-)
+/*
+#include "sqlite3-binding.h"
+
+int sqlite3_series_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi);
+
+static int load_misc_extensions() {
+	return sqlite3_auto_extension((void *)sqlite3_series_init);
+}
+*/
+import "C"
 
 func init() {
-	misc.LoadMiscExtensions()
+	C.load_misc_extensions()
 }
